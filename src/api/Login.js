2f91df74f940dbb793d666232f7dcc9ba4207618
garimabeli-dot <61051726+garimabeli-dot@users.noreply.github.com>
2020-02-24 22:43:56 +0530

    Update Login.js

--- src/api/Login.js
+++ src/api/Login.js
@@ -29,3 +29,16 @@ export function postReviews(url, method,header=[],body='') {
             .catch(err => reject(err.message));
     });
 }
+
+export function postProfileDetails(url, method,header=[],body='') {
+    return new Promise((resolve, reject) => {
+        fetch(url,{ 
+            method: method,
+            headers:header,
+            body: (body)
+         })
+            .then(handleErrors)
+            .then(data => resolve(data))
+            .catch(err => reject(err.message));
+    });
+}
